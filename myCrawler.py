32f4daa13f2fb4601e4f328b389b328e99005585
castaway <apollonas@csd.auth.gr>
2020-01-25 23:24:24 +0200

    visible text and links

--- myCrawler.py
+++ myCrawler.py
@@ -1,11 +1,20 @@
 import sys
 import requests
 from bs4 import BeautifulSoup
+from bs4.element import Comment
+
+
+def tag_visible(element):
+    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+        return False
+    if isinstance(element, Comment):
+        return False
+    return True
 
 
 if __name__ == "__main__":
     numOfParameters = len(sys.argv)
-    if(numOfParameters<3):
+    if(numOfParameters<4):
         print("Parameters Error")
         sys.exit()
 
@@ -13,13 +22,25 @@ if __name__ == "__main__":
     for i in range (1, numOfParameters):
         parametersList.append(sys.argv[i])
 
+    # starting page
     startingPage = requests.get(parametersList[0])
+    # number of pages to crawl
     pagesToCrawl = int(parametersList[1])
-    if(len(parametersList)>2):
-        numOfThreads = parametersList[2]
+    # 0 doesnt keep data from previous crawl, 1 keeps it
+    keep = int(parametersList[2])
+    # number of threads
+    if(len(parametersList)>3):
+        numOfThreads = parametersList[3]
 
     soup = BeautifulSoup(startingPage.content, 'html.parser')
-    print(soup.prettify())
+    texts = soup.findAll(text=True)
+    visible_texts = filter(tag_visible, texts)
+    links = [a.get('href') for a in soup.find_all('a', href=True)]
+    # for l in links:
+    #     print(l)
+    final = u" ".join(t.strip() for t in visible_texts)
+    print(final)
+    print(type(final))
     # a = int(sys.argv[1])
     # b = int(sys.argv[2])
     # print(a)
