6f622b7caaa4e3ce96289868f5e1fb4e5089b47a
Apoll <apollonas@csd.auth.gr>
2020-01-27 06:28:21 +0200

    ...

--- myCrawler.py
+++ myCrawler.py
@@ -2,6 +2,17 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 from bs4.element import Comment
+from urllib.parse import urlparse
+from multiprocessing.pool import ThreadPool as Pool
+
+
+# define worker function before a Pool is instantiated
+def worker(item):
+    try:
+        print(item)
+    except:
+        print('error with item')
+
 
 
 def tag_visible(element):
@@ -30,17 +41,31 @@ if __name__ == "__main__":
     keep = int(parametersList[2])
     # number of threads
     if(len(parametersList)>3):
-        numOfThreads = parametersList[3]
+        numOfThreads = int(parametersList[3])
+
+    pool = Pool(numOfThreads)
+
 
     soup = BeautifulSoup(startingPage.content, 'html.parser')
     texts = soup.findAll(text=True)
     visible_texts = filter(tag_visible, texts)
     links = [a.get('href') for a in soup.find_all('a', href=True)]
+
+    for item in links:
+        pool.apply_async(worker, (item,))
+
+    pool.close()
+    pool.join()
+
+    # parsed_uri = urlparse(parametersList[0])
+    # result = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
+    # print(result)
+
     # for l in links:
     #     print(l)
     final = u" ".join(t.strip() for t in visible_texts)
-    print(final)
-    print(type(final))
+    # print(final)
+    # print(type(final))
     # a = int(sys.argv[1])
     # b = int(sys.argv[2])
     # print(a)
